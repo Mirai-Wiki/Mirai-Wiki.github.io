787ad1e45dd8bab96e6f9177c352fbb8c8f7d93a
Ggcpp <gg.geinoz@outlook.com>
2022-06-25 20:41:20 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,227 @@
+window.history.scrollRestoration = "manual";
+
+const ARTICLE_KEY = "article-scroll-y";
+const MENU_KEY = "menu-scroll-y";
+
+const pageContent = document.querySelector("#page-content");
+const article = document.querySelector("#page-content-center-column");
+const menu = document.querySelector(".page-name-container");
+const searchBar = document.querySelector(".search-bar");
+const searchResult = document.querySelector(".search-result-container");
+const tocList = document.querySelector(".toc-container");
+
+// Manage scrolling
+article.addEventListener("scroll", (evt) => {
+    window.sessionStorage.setItem(ARTICLE_KEY, evt.target.scrollTop);
+});
+
+menu.addEventListener("scroll", (evt) => {
+    window.sessionStorage.setItem(MENU_KEY, evt.target.scrollTop);
+});
+
+window.addEventListener("load", () => {
+    const articleScrollY = sessionStorage.getItem(ARTICLE_KEY) || 0;
+    const menuScrollY = sessionStorage.getItem(MENU_KEY) || 0;
+
+    article.scrollTo(0, articleScrollY);
+    menu.scrollTo(0, menuScrollY);
+});
+
+// Load menu (json)
+window.addEventListener("load", async () => {
+    const res = await fetch("data.json");
+    const states = await res.json();
+
+    states.forEach((value) => {
+        const element = document.createElement("li");
+        element.classList.add("page-name-item");
+        element.innerHTML = value.name.french;
+
+        menu.querySelector("ol").appendChild(element);
+    });
+});
+
+// Searching
+window.addEventListener("click", () => {
+    searchResult.hidden = true;
+});
+
+searchBar.addEventListener("input", () => {
+    const allPages = menu.querySelectorAll(".page-name-item");
+    const lstPages = [...allPages].map(e=>e.innerHTML);
+
+    if (searchBar.value.length > 0)
+    {
+        searchResult.hidden = false;
+        const rect = searchBar.getBoundingClientRect();
+        const posX = rect.x + 20;
+        const posY = rect.y + 20;
+        searchResult.style.left = `${posX}px`;
+        searchResult.style.top = `${posY}px`;
+
+        let matches = lstPages.filter((state) => {
+            const regex = new RegExp(searchBar.value, "gi");
+            return state.match(regex);
+        });
+
+        if (matches.length > 0)
+        {
+            const html = matches.map(match => `
+                <p><a href=${match}.html>${match}</a></p>
+            `).join('');
+
+            searchResult.innerHTML = html;
+        }
+        else
+        {
+            searchResult.innerHTML = "<p>No result...</p>";
+        }
+    }
+    else
+    {
+        searchResult.hidden = true;
+        searchResult.innerHTML = '';
+    }
+});
+
+// Preview
+const sleep = ms => new Promise(res => setTimeout(res, ms));
+const eventEmitter = new EventEmitter();
+
+async function loadPageHTML(pageContainer, fileName)
+{
+    const res = await fetch(fileName + ".html");
+
+    if (res.status !== 404)
+    {
+        await res.text().then((html) => {
+            pageContainer.innerHTML = html;
+        });
+    }
+    else
+    {
+        pageContainer.innerHTML = `
+            <p>Erreur 404, la page ${fileName}.html n'existe pas :'(</p>
+        `;
+    }
+}
+
+function removePreviews()
+{
+    const allPreviews = pageContent.querySelectorAll(".page-preview");
+    [...allPreviews].map((e) => {
+        e.parentNode.removeChild(e);
+    });
+}
+
+async function popupPreview(evt)
+{
+    const link = evt.target;
+    const linkBox = link.getBoundingClientRect();
+    const previewContainer = document.createElement("div");
+    previewContainer.classList.add("page-preview");
+    previewContainer.classList.add("page-container");
+    previewContainer.style = `
+        position: absolute;
+        top: ${linkBox.bottom}px;
+        left: ${evt.clientX - 10}px;
+        width: 400px;
+        height: 500px;
+        background-color: white;
+    `;
+
+    pageContent.appendChild(previewContainer);
+
+    previewContainer.addEventListener("mouseleave", (evt) => {
+        const currentPreview = evt.target;
+        const previewBox = currentPreview.getBoundingClientRect();
+
+        if (evt.clientX >= previewBox.x && evt.clientX < previewBox.x + previewBox.width
+            && evt.clientY >= previewBox.y && evt.clientY < previewBox.y + previewBox.height)
+        {
+        }
+        else
+        {
+            currentPreview.parentNode.removeChild(currentPreview);
+            const allPreviews = pageContent.querySelectorAll(".page-preview");
+            const previewArray = [...allPreviews];
+
+            if (previewArray.length === 0) { return; }
+
+            const fallbackPreview = previewArray[previewArray.length - 1];
+            const fallbackPreviewBox = fallbackPreview.getBoundingClientRect();
+
+            if (evt.clientX >= fallbackPreviewBox.x && evt.clientX < fallbackPreviewBox.x + fallbackPreviewBox.width
+                && evt.clientY >= fallbackPreviewBox.y && evt.clientY < fallbackPreviewBox.y + fallbackPreviewBox.height)
+            {
+            }
+            else
+            {
+                removePreviews();
+            }
+        }
+    });
+
+    const fileName = link.hash.slice(2);
+    await loadPageHTML(previewContainer, fileName);
+    addEventOnPageLinks(previewContainer);
+}
+
+function addEventOnPageLinks(pageContainer)
+{
+    const allArticleLinks = pageContainer.querySelectorAll(".page-link");
+    [...allArticleLinks].map((e) => {
+        e.addEventListener("mouseover", popupPreview);
+        e.addEventListener("mouseleave", (evt) => {
+            const allPreviews = pageContent.querySelectorAll(".page-preview");
+            const previewArray = [...allPreviews];
+
+            const currentPreview = previewArray[previewArray.length - 1];
+            const previewBox = currentPreview.getBoundingClientRect();
+
+            if (evt.clientX >= previewBox.x && evt.clientX < previewBox.x + previewBox.width
+                && evt.clientY >= previewBox.y && evt.clientY < previewBox.y + previewBox.height)
+            {
+
+            }
+            else
+            {
+                currentPreview.parentNode.removeChild(currentPreview);
+            }
+        });
+    });
+}
+
+// Table of content
+function updateTOC()
+{
+    tocList.querySelector("ul").innerHTML = '';
+
+    const allTitles = article.querySelectorAll("h1, h2, h3, h4, h5, h6");
+    console.log(allTitles);
+    [...allTitles].map((e) => {
+        const item = document.createElement("li");
+        item.classList.add("toc-item-" + e.localName.toLowerCase());
+        item.innerHTML = e.innerHTML;
+
+        tocList.querySelector("ul").appendChild(item);
+    });
+}
+
+// Rooter
+window.addEventListener("load", async () => {
+    removePreviews();
+    const fileName = window.location.hash.slice(2) || 'homepage';
+    
+    await loadPageHTML(article, fileName);
+    addEventOnPageLinks(article);
+    updateTOC();
+});
+window.addEventListener("hashchange", async () => {
+    removePreviews();
+    const fileName = window.location.hash.slice(2) || 'homepage';
+    
+    await loadPageHTML(article, fileName);
+    addEventOnPageLinks(article);
+    updateTOC();
+});
