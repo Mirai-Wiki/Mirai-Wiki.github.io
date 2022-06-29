420675b4cb60fac1fb45cda4cb102d8839a259ac
Ggcpp <gg.geinoz@outlook.com>
2022-06-29 18:50:19 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -343,3 +343,13 @@ window.addEventListener("hashchange", async () => {
     updateTOC();
 });
 
+// PWA service worker
+if ("serviceWorker" in navigator)
+{
+    window.addEventListener("load", () => {
+        navigator.serviceWorker
+            .register("/serviceWorker.js")
+            .then((res) => console.log("serviceWorker ok"))
+            .catch((err) => console.log("serviceWorker not ok"));
+    });
+}
