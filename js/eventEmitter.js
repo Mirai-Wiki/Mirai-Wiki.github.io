f788209466a42f7a6f9591ac19de7e98cac03eb7
Ggcpp <gg.geinoz@outlook.com>
2022-06-30 10:19:44 +0200

    new architecture

---
+++ js/eventEmitter.js
@@ -0,0 +1,30 @@
+class EventEmitter {
+    constructor()
+    {
+        this._events = {};
+    }
+
+    on(name, listener)
+    {
+        if (!this._events[name])
+        {
+            this._events[name] = [];
+        }
+
+        this._events[name].push(listener);
+    }
+
+    emit(name, data)
+    {
+        if (!this._events[name])
+        {
+            throw new Error(`Error EventEmitter: ${name} event does not exist!`);
+        }
+
+        const fireCallback = (callback) => {
+            callback(data);
+        };
+
+        this._events[name].forEach(fireCallback);
+    }
+}
