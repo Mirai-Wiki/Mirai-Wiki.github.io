38f757db1eef9c42a0460aa291763251ab10331c
Ggcpp <gg.geinoz@outlook.com>
2022-07-01 14:21:27 +0200

    update

--- gen_json.sh
+++ gen_json.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 echo '$meta-json$' > /tmp/metadata.pandoc-tpl
-#pandoc test.md --template=/tmp/metadata.pandoc-tpl | jq
 
-for i in $(find ./markdown -name "*.md" -not -path "./markdown/Excalidraw/*" -not -path "./markdown/filters/*" -not -path "./markdown/templates/*")
+find ./markdown -name "*.md" -not -path "./markdown/Excalidraw/*" -not -path "./markdown/filters/*" -not -path "./markdown/templates/*" |
+while read line ;
 do
-    echo $i
-    #pandoc $i --template=/tmp/metadata.pandoc-tpl | jq
+pandoc "$line" --template=/tmp/metadata.pandoc-tpl | jq
 done;
