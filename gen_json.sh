dab871fea70ef8690dc6755408c37c8c675886b9
Ggcpp <gg.geinoz@outlook.com>
2022-07-01 20:28:12 +0200

    update

--- gen_json.sh
+++ gen_json.sh
@@ -25,3 +25,14 @@ echo "]" >> ./data/pages.json
 rm ./data/temp.json
 
 echo "JSON Generation: Successful!"
+
+#pandoc "$file" --template=/tmp/metadata.pandoc-tpl |   > {"field":"value"}
+#sed 's/.$//' | sed 's/"/\\"/g' |                       > 'delete last char' | 'escape the quotes'
+#xargs -I {} echo '{},'$(dirname "$file" |              > 
+#cut -d '/' -f3- | rev | cut -f 2- -d '.' | rev |       > 'cut the two first dirs'
+#xargs -I {} echo '"folder":"{}",') |                   >
+#sed 's/.$//' |                                         > 'delete the pending , at the end'
+#xargs -0 -I {} echo {}'}' |                            > 'add } at the end'
+#jq >> ./data/temp.json                                 > 'format the input to json'
+
+#head -n -1 ./data/temp.json > ./data/pages.json        > 'remove the last line'
