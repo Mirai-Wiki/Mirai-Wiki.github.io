790ff69f26cf9dd920cccbe36422395323a04768
Ggcpp <gg.geinoz@outlook.com>
2022-07-01 17:17:05 +0200

    update

--- gen_json.sh
+++ gen_json.sh
@@ -6,11 +6,14 @@ echo '$meta-json$' > /tmp/metadata.pandoc-tpl
 
 echo "[" > ./data/temp.json
 find ./markdown -name "*.md" -not -path "./markdown/Excalidraw/*" -not -path "./markdown/filters/*" -not -path "./markdown/templates/*" -not -path "./markdown/homepage.md" |
-while read line ;
+sort |
+while read file ;
 do
-    pandoc "$line" --template=/tmp/metadata.pandoc-tpl | jq >> ./data/temp.json
+    pandoc "$file" --template=/tmp/metadata.pandoc-tpl | jq >> ./data/temp.json
     echo ',' >> ./data/temp.json
 done;
 head -n -1 ./data/temp.json > ./data/pages.json
 echo "]" >> ./data/pages.json
 rm ./data/temp.json
+
+echo "JSON Generation: Successful!"
--- markdown/homepage.md
+++ markdown/homepage.md
@@ -1,3 +1,7 @@
+---
+title: homepage
+---
+
 # Page d'accueil
 ## Bienvenue
 Ce site a été conçu afin de regrouper tout un tas de connaissances ! Comme il est encore en construction (intensif), le site ne contient que très peu de pages. Le but sur le moyen terme est d'injecter massivement des pages dans le site et de les relier entre-elles par des liens... exactement comme Wikipédia.
